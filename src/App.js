90770bc3a3e63a6d7c84e8ce73c06220b8eeaa91
mishraprajwal <prajwalm882@gmail.com>
2025-04-02 18:52:22 -0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1>Hello, React!</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <p>{count}</p>
+      <button onClick={() => setCount(count - 1)}>-</button>
     </div>
   );
 }
